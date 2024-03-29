2c48c0020c0819cf4854d48068b00a25dc952f23
Travis <9389564+tn-wright@users.noreply.github.com>
2024-03-29 12:17:55 -0500

    Updating website

--- javascript/index.js
+++ javascript/index.js
@@ -1,157 +1,323 @@
-import Highway from '@dogstudio/highway';
-import Transitioner from './transitioner';
-import ContactRenderer from './contact-renderer';
-import { TimelineLite, Back } from 'gsap';
-import highway from '@dogstudio/highway';
+import Highway from "@dogstudio/highway";
+import Transitioner from "./transitioner";
+import { gsap } from "gsap";
+
+const websiteDomainWithScheme = "http://localhost:1234/";
 
 const highwayCore = new Highway.Core({
-    transitions: {
-        default: Transitioner
-    },
-    renderers: {
-        contact: ContactRenderer
-    }
+  transitions: {
+    default: Transitioner,
+  },
 });
 /**
- * Add the on click listener to the hamburger button that determines if it submenu should be loaded or unloaded
+ * Add the on click listener to the hamburger button that determines if the submenu should be loaded or unloaded
  */
 function setUpHamburger() {
-    const hamburger = document.getElementById("hamburger-button");
+  const hamburger = document.getElementById("hamburger-button");
 
-    hamburger.onclick = function() {
-        hamburger.setAttribute('disabled', true);
-        const hamburgerContent = document.getElementById("hamburger-content");
-        const opacity = hamburgerContent.style.opacity;
-        const navOverlay = document.getElementById("nav-overlay");
+  hamburger.onclick = function () {
+    hamburger.setAttribute("disabled", true);
+    const hamburgerContent = document.getElementById("hamburger-content");
+    const opacity = hamburgerContent.style.opacity;
+    const navOverlay = document.getElementById("nav-overlay");
 
-        if(opacity === '1') {
-            fadeOutHamburgerContent(hamburger, hamburgerContent, navOverlay);
-        } else {
-            fadeInHamburgerContent(hamburger, hamburgerContent, navOverlay);
-        }
-    };
+    if (opacity === "1") {
+      fadeOutHamburgerContent(hamburger, hamburgerContent, navOverlay);
+    } else {
+      fadeInHamburgerContent(hamburger, hamburgerContent, navOverlay);
+    }
+  };
 }
 
 /**
  * Add an onclick listener to the document to close the nav menu if any click occurs outside of it
- * 
+ *
  * @param {MouseEvent} event - The triggering event object
  */
-document.onclick = function(event) {
-    const hamburger = document.getElementById("hamburger-button");
-    const hamburgerContent = document.getElementById("hamburger-content");
-    const hamburgerOpacity = hamburgerContent.style.opacity;
-    const navOverlay = document.getElementById("nav-overlay");
+document.onclick = function (event) {
+  const hamburger = document.getElementById("hamburger-button");
+  const hamburgerContent = document.getElementById("hamburger-content");
+  const hamburgerOpacity = hamburgerContent.style.opacity;
+  const navOverlay = document.getElementById("nav-overlay");
 
-    if(hamburgerOpacity === '1') {
-        event.preventDefault();
-        fadeOutHamburgerContent(hamburger, hamburgerContent, navOverlay);
-        return;
-    }
-}
+  //Immediately exit this function and perform a normal click if we are clicking one of the nav icons
+  //  We want to preserve their functionality in all cases, even if the hamburger is open
+  if (event.target.classList.contains("nav-icon")) {
+    return;
+  }
+
+  if (hamburgerOpacity === "1") {
+    event.preventDefault();
+    fadeOutHamburgerContent(hamburger, hamburgerContent, navOverlay);
+    return;
+  }
+};
 
 /**
  * An event handling function that find elements and passes them, along with the event object, to trapTabFocus
  * @param {KeyboardEvent} event - the triggering event
  */
 function trapTabFocusWrapper(event) {
-    const hamburger = document.getElementById("hamburger-button");
-    const hamburgerContent = document.getElementById("hamburger-content");
-    trapTabFocus(event, hamburger, hamburgerContent);
+  const hamburger = document.getElementById("hamburger-button");
+  const hamburgerContent = document.getElementById("hamburger-content");
+  trapTabFocus(event, hamburger, hamburgerContent);
 }
 
 /**
  * Traps tab navigation within the hamburger submenu when it is shown
- * 
+ *
  * @param {KeyboardEvent} e - the triggering keypress event
  * @param {HTMLElement} hamburger - the hamburger button element
  * @param {HTMLElement} hamburgerContent - the div that wraps the hamburger submenu
  */
 function trapTabFocus(e, hamburger, hamburgerContent) {
-    let isTabPressed = e.key === 'Tab';
+  let isTabPressed = e.key === "Tab";
 
-    //If the key pressed was not tab, return
-    if(!isTabPressed) {
-        return;
+  //If the key pressed was not tab, return
+  if (!isTabPressed) {
+    return;
+  }
+
+  //Get the elements withing the hamburger submenu and mark the last one as lastFocusableElement
+  const tabbableElements = hamburgerContent.querySelectorAll(".hamburger-link");
+  const lastFocusableElement = tabbableElements[tabbableElements.length - 1];
+
+  //If shift was held, we need to move backwards
+  if (e.shiftKey) {
+    if (document.activeElement === hamburger) {
+      //If moving backwards from the first element (the hamburger button), go to the last nav element
+      lastFocusableElement.focus();
+      e.preventDefault();
     }
-
-    //Get the elements withing the hamburger submenu and mark the last one as lastFocusableElement
-    const tabbableElements = hamburgerContent.querySelectorAll(".hamburger-link");
-    const lastFocusableElement = tabbableElements[tabbableElements.length - 1];
-
-    //If shift was held, we need to move backwards
-    if(e.shiftKey) {
-        if(document.activeElement === hamburger) {
-            //If moving backwards from the first element (the hamburger button), go to the last nav element
-            lastFocusableElement.focus()
-            e.preventDefault();
-        }
-    } else {
-        if(document.activeElement === lastFocusableElement) {
-            //If moving forward from the last nav element, wrap around to the beginning
-            hamburger.focus();
-            e.preventDefault();
-        }
+  } else {
+    if (document.activeElement === lastFocusableElement) {
+      //If moving forward from the last nav element, wrap around to the beginning
+      hamburger.focus();
+      e.preventDefault();
     }
+  }
 }
 
 /**
  * Fade out the hamburger menu and the page overlay, and set the page state back to the defaults
- * 
+ *
  * @param {HTMLElement} hamburger - the hamburger button
  * @param {HTMLElement} hamburgerContent - the div containing the hamburger submenu
  * @param {HTMLElement} navOverlay - the overlay div
  */
 function fadeOutHamburgerContent(hamburger, hamburgerContent, navOverlay) {
-    //animate hamburger menu and the overlay of the rest of the page
-    let timeline = new TimelineLite();
-    timeline.to(hamburgerContent, {opacity: 0, duration: 0.3, ease: Back.easeOut, onComplete: () => {
-        hamburger.removeAttribute('disabled');
-        hamburgerContent.style.pointerEvents = 'none';
-        navOverlay.style.pointerEvents = 'none';
+  //animate hamburger menu and the overlay of the rest of the page
+  let timeline = new gsap.timeline();
+  timeline
+    .to(hamburgerContent, {
+      opacity: 0,
+      duration: 0.3,
+      ease: "back.out()",
+      onComplete: () => {
+        hamburger.removeAttribute("disabled");
+        hamburgerContent.style.pointerEvents = "none";
+        navOverlay.style.pointerEvents = "none";
 
         //disable tab targetting of navigation menu items since it is gone
-        hamburgerContent.querySelectorAll(".hamburger-link").forEach(element => {
-            element.setAttribute('tabindex', '-1');
-        });
+        hamburgerContent
+          .querySelectorAll(".hamburger-link")
+          .forEach((element) => {
+            element.setAttribute("tabindex", "-1");
+          });
 
         //remove the tab trapping event listener since the navigation menu is gone
-        document.removeEventListener('keydown', trapTabFocusWrapper)
-    }}).to(navOverlay, {opacity: 0, duration: 0.3, ease: Back.easeOut}, "-=0.3");
+        document.removeEventListener("keydown", trapTabFocusWrapper);
+      },
+    })
+    .to(navOverlay, { opacity: 0, duration: 0.3, ease: "back.out()" }, "-=0.3");
 }
 
 /**
  * Fade in the hamburger menu and the page overlay, and set the page state accordingly
- * 
+ *
  * @param {HTMLElement} hamburger - the hamburger button
  * @param {HTMLElement} hamburgerContent - the div containing the hamburger submenu
  * @param {HTMLElement} navOverlay - the overlay div
  */
 function fadeInHamburgerContent(hamburger, hamburgerContent, navOverlay) {
-    //animate hamburger menu and the overlay of the rest of the page
-    let timeline = new TimelineLite();
-    timeline.to(hamburgerContent, {opacity: 1, duration: 0.3, ease: Back.easeOut, onComplete: () => {
+  //animate hamburger menu and the overlay of the rest of the page
+  let timeline = new gsap.timeline();
+  timeline
+    .to(hamburgerContent, {
+      opacity: 1,
+      duration: 0.3,
+      ease: "back.out()",
+      onComplete: () => {
         //Allow pointer events now that content is visible
-        hamburgerContent.style.pointerEvents = 'auto';
-        navOverlay.style.pointerEvents = 'auto';
+        hamburgerContent.style.pointerEvents = "auto";
+        navOverlay.style.pointerEvents = "auto";
 
         //Allow the navigation items to be tab targetted
-        hamburgerContent.querySelectorAll(".hamburger-link").forEach(element => {
-            element.setAttribute('tabindex', '0');
-        });
+        hamburgerContent
+          .querySelectorAll(".hamburger-link")
+          .forEach((element) => {
+            element.setAttribute("tabindex", "0");
+          });
 
         //add the listener to trap tab in the navigation menu
-        document.addEventListener('keydown', trapTabFocusWrapper);
+        document.addEventListener("keydown", trapTabFocusWrapper);
 
         //after all of the animation and setup is complete, re-enable the hamburger button
-        hamburger.removeAttribute('disabled');
-    }}).to(navOverlay, {opacity: 1, duration: 0.3, ease: Back.easeOut}, "-=0.3");
+        hamburger.removeAttribute("disabled");
+      },
+    })
+    .to(navOverlay, { opacity: 1, duration: 0.3, ease: "back.out()" }, "-=0.3");
 }
 
-//call the setup funciton
+/**
+ * Move a page to the left using Highway
+ */
+function movePageLeft() {
+  var linkElement;
+  var locationString = window.location.href;
+
+  if (locationString === websiteDomainWithScheme + "portfolio.html") {
+    var element = document.getElementById("aboutLink");
+    highwayCore.redirect(
+      websiteDomainWithScheme + "about.html",
+      false,
+      element
+    );
+  } else if (locationString === websiteDomainWithScheme + "about.html") {
+    var element = document.getElementById("indexLink");
+    highwayCore.redirect(websiteDomainWithScheme, false, element);
+  } else if (
+    locationString === websiteDomainWithScheme ||
+    locationString === websiteDomainWithScheme + "index.html"
+  ) {
+    var element = document.getElementById("portfolioLink");
+    highwayCore.redirect(
+      websiteDomainWithScheme + "portfolio.html",
+      false,
+      element
+    );
+  }
+}
+
+/**
+ * Move a page to the right using Highway
+ */
+function movePageRight() {
+  var linkElement;
+  var locationString = window.location.href;
+
+  if (locationString === websiteDomainWithScheme + "about.html") {
+    var element = document.getElementById("portfolioLink");
+    highwayCore.redirect(
+      websiteDomainWithScheme + "portfolio.html",
+      false,
+      element
+    );
+  } else if (locationString === websiteDomainWithScheme + "portfolio.html") {
+    var element = document.getElementById("indexLink");
+    highwayCore.redirect(websiteDomainWithScheme, false, element);
+  } else if (
+    locationString === websiteDomainWithScheme ||
+    locationString === websiteDomainWithScheme + "index.html"
+  ) {
+    var element = document.getElementById("aboutLink");
+    highwayCore.redirect(
+      websiteDomainWithScheme + "about.html",
+      false,
+      element
+    );
+  }
+}
+
+//call the setup funciton for the hamburger menu
 setUpHamburger();
 
-document.addEventListener("DOMContentLoaded", function() {
-    //Initialize overlay scrollbars on DOM load
-	OverlayScrollbars(document.querySelectorAll("main"), { className: "os-theme-light", scrollbars: { autoHide: "scroll", autoHideDelay: 250} });
-})
+//Configure the OverlayScrollbars library
+document.addEventListener("DOMContentLoaded", function () {
+  //Initialize overlay scrollbars on DOM load
+  OverlayScrollbars(document.querySelectorAll("main"), {
+    className: "os-theme-light",
+    scrollbars: { autoHide: "scroll", autoHideDelay: 250 },
+  });
+});
+
+var keyBuffer = []; //Holds key presses for the konami code
+const konamiCode = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+  "Enter",
+];
+const konamiKeySet = [
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "a",
+  "b",
+  "Enter",
+];
+
+//Check the keyBuffer array to see if it matches the konami code
+function checkForKonamiCode() {
+  //Check each key against the corresponding entry in the code
+  for (i = 0; i < konamiCode.length; i++) {
+    if (keyBuffer[i] !== konamiCode[i]) {
+      return;
+    }
+  }
+
+  //Clear the buffer
+  keyBuffer = [];
+
+  //Animation settings
+  const konamiSlide = [{ transform: "translate(0%, 250%)" }];
+  const konamiTiming = { duration: 3000, iterations: 1 };
+
+  //Animate the secret
+  let konamiContainer = document.getElementById("konamiContainer");
+  konamiContainer.animate(konamiSlide, konamiTiming);
+}
+
+//Add event listener for keydown, to handle changing pages with arrow keys
+document.addEventListener("keyup", (event) => {
+  if (konamiKeySet.includes(event.key)) {
+    keyBuffer.push(event.key);
+
+    if (keyBuffer.length > 11) {
+      console.log("Shifting buffer");
+      keyBuffer.shift();
+      console.log(keyBuffer);
+    }
+  } else {
+    //If something not part of the code is pressed, reset the progress on the code
+    keyBuffer = [];
+  }
+
+  //Left and right arrow keys can be used to change the page
+  if (event.key === "ArrowRight") {
+    movePageRight();
+  } else if (event.key === "ArrowLeft") {
+    movePageLeft();
+  }
+
+  //Check the key buffer to see if we have entered the konami code
+  checkForKonamiCode();
+});
+
+//Set up Hammer.JS event handlers to handle swiping gestures
+var hammer = new Hammer.Manager(document);
+hammer.add(
+  new Hammer.Swipe({ direction: Hammer.DIRECTION_HORIZONTAL, velocity: 5 })
+);
+
+//Reversing the direction makes it behave like dragging the page
+hammer.on("swipeleft", movePageRight);
+hammer.on("swiperight", movePageLeft);
--- javascript/transitioner.js
+++ javascript/transitioner.js
@@ -1,168 +1,169 @@
+import Highway from "@dogstudio/highway";
 import { TimelineLite, Back } from "gsap";
-import Highway from '@dogstudio/highway';
 
 /**
  * The transitioner for the website that determines which animations to use based on the pages
  * involved in the transition.
  */
 class Transitioner extends Highway.Transition {
+  /**
+   * Slide the page elements left, transition from "from" to "to", calling done when finished
+   *
+   * @param {*} from - the section we are navigation from
+   * @param {*} to - the section we are navigating to
+   * @param {*} done - the done callback
+   */
+  _slideLeft(from, to, done) {
+    this._scrollToTop();
+    var t1 = new TimelineLite();
+    t1.to(from, {
+      duration: 0.4,
+      left: "-100%",
+      ease: Back.easeOut.config(0.8),
+    }).fromTo(
+      to,
+      { left: "100%" },
+      {
+        duration: 0.4,
+        left: "0%",
+        ease: Back.easeOut.config(0.8),
+        onComplete: () => {
+          from.remove();
+          done();
+        },
+      },
+      "-=0.3"
+    );
+  }
 
-    /**
-     * Slide the page elements up, transition from "from" to "to", calling done when finished
-     * 
-     * @param {*} from - the section we are navigation from
-     * @param {*} to - the section we are navigating to
-     * @param {*} done - the done callback
-     */
-    _slideUp(from, to, done) {
-        this._scrollToTop();
-        var t1 = new TimelineLite();
-        t1.to(from, {duration: 0.4, top: '-100%', ease: Back.easeOut.config(0.8)})
-          .fromTo(to, {top: '100%'}, {duration: 0.4, top: '0%', ease: Back.easeOut.config(0.8), onComplete: () => {
-            from.remove();
-            done();
-        }}, "-=0.3");
+  /**
+   * Slide the page elements right, transition from "from" to "to", calling done when finished
+   *
+   * @param {*} from - the section we are navigation from
+   * @param {*} to - the section we are navigating to
+   * @param {*} done - the done callback
+   */
+  _slideRight(from, to, done) {
+    this._scrollToTop();
+    var t1 = new TimelineLite();
+    t1.to(from, {
+      duration: 0.4,
+      left: "100%",
+      ease: Back.easeOut.config(0.8),
+    }).fromTo(
+      to,
+      { left: "-100%" },
+      {
+        duration: 0.4,
+        left: "0%",
+        ease: Back.easeOut.config(0.8),
+        onComplete: () => {
+          from.remove();
+          done();
+        },
+      },
+      "-=0.3"
+    );
+  }
+
+  /**
+   * cross fade the page elements, transition from "from" to "to", calling done when finished
+   *
+   * @param {*} from - the section we are navigation from
+   * @param {*} to - the section we are navigating to
+   * @param {*} done - the done callback
+   */
+  _fade(from, to, done) {
+    this._scrollToTop();
+    var t1 = new TimelineLite();
+    t1.to(from, { duration: 0.1, opacity: "0" }).fromTo(
+      to,
+      { opacity: "0" },
+      {
+        duration: 0.1,
+        opacity: "1",
+        onComplete: () => {
+          from.remove();
+          done();
+        },
+      }
+    );
+  }
+
+  /**
+   * Reset the scroll to the top of the page
+   */
+  _scrollToTop() {
+    if ("scrollRestoration" in history) {
+      history.scrollRestoration = "manual";
     }
 
-    /**
-     * Slide the page elements down, transition from "from" to "to", calling done when finished
-     * 
-     * @param {*} from - the section we are navigation from
-     * @param {*} to - the section we are navigating to
-     * @param {*} done - the done callback
-     */
-    _slideDown(from, to, done) {
-        this._scrollToTop();
-        var t1 = new TimelineLite();
-        t1.to(from, {duration: 0.4, top: '100%', ease: Back.easeOut.config(0.8)})
-          .fromTo(to, {top: '-100%'}, {duration: 0.4, top: '0%', ease: Back.easeOut.config(0.8), onComplete: () => {
-            from.remove();
-            done();
-        }}, "-=0.3");
-    }
+    window.scrollTo(0, 0);
+  }
 
-    /**
-     * Slide the page elements left, transition from "from" to "to", calling done when finished
-     * 
-     * @param {*} from - the section we are navigation from
-     * @param {*} to - the section we are navigating to
-     * @param {*} done - the done callback
-     */
-    _slideLeft(from, to, done) {
-        this._scrollToTop();
-        var t1 = new TimelineLite();
-        t1.to(from, {duration: 0.4, left: '-100%', ease: Back.easeOut.config(0.8)})
-          .fromTo(to, {left: '100%'}, {duration: 0.4, left: '0%', ease: Back.easeOut.config(0.8), onComplete: () => {
-            from.remove();
-            done();
-        }}, "-=0.3");
-    }
+  /**
+   * Called when transitioning into a page
+   *
+   * @param {*} from - the section being transitioned from
+   * @param {*} to - the section being transitioned to
+   * @param {*} done - the done callback
+   * @param {*} trigger - The trigger of the transition
+   */
+  in({ from, to, done, trigger }) {
+    //If the transition was triggered by the back/forward buttons or a link not in the nav bar
+    if (
+      trigger === "popstate" ||
+      (!trigger.classList.contains("hamburger-link") &&
+        !trigger.classList.contains("logo-link"))
+    ) {
+      //get the names of the to and from sections
+      const toName = to.getAttribute("data-router-view");
+      const fromName = from.getAttribute("data-router-view");
 
-    /**
-     * Slide the page elements right, transition from "from" to "to", calling done when finished
-     * 
-     * @param {*} from - the section we are navigation from
-     * @param {*} to - the section we are navigating to
-     * @param {*} done - the done callback
-     */
-    _slideRight(from, to, done) {
-        this._scrollToTop();
-        var t1 = new TimelineLite();
-        t1.to(from, {duration: 0.4, left: '100%', ease: Back.easeOut.config(0.8)})
-          .fromTo(to, {left: '-100%'}, {duration: 0.4, left: '0%', ease: Back.easeOut.config(0.8), onComplete: () => {
-            from.remove();
-            done();
-        }}, "-=0.3");
-    }
-    
-    /**
-     * cross fade the page elements, transition from "from" to "to", calling done when finished
-     * 
-     * @param {*} from - the section we are navigation from
-     * @param {*} to - the section we are navigating to
-     * @param {*} done - the done callback
-     */
-    _fade(from, to, done) {
-        this._scrollToTop();
-        var t1 = new TimelineLite();
-        t1.to(from, {duration: 0.1, opacity: '0'})
-            .fromTo(to, {opacity: '0'}, {duration: 0.1, opacity: '1', onComplete: () => {
-            from.remove();
-            done();
-        }});
-    }
+      //Create booleans regarding the to namme
+      const isToHome = toName === "home";
+      const isToAbout = toName === "about";
+      const isToPortfolio = toName === "portfolio";
 
-    /**
-     * Reset the scroll to the top of the page
-     */
-    _scrollToTop() {
-        if('scrollRestoration' in history) {
-            history.scrollRestoration = 'manual';
-        }
+      //create booleans regarding the from name
+      const isFromHome = fromName === "home";
+      const isFromAbout = fromName === "about";
+      const isFromPortfolio = fromName === "portfolio";
 
-        window.scrollTo(0,0);
+      //determine the animation based on the to and from pages
+      if (
+        (isFromHome && isToPortfolio) ||
+        (isFromPortfolio && isToAbout) ||
+        (isFromAbout && isToHome)
+      ) {
+        //right
+        this._slideRight(from, to, done);
+      } else if (
+        (isFromHome && isToAbout) ||
+        (isFromAbout && isToPortfolio) ||
+        (isFromPortfolio && isToHome)
+      ) {
+        //left
+        this._slideLeft(from, to, done);
+      } else {
+        //default to fade
+        this._fade(from, to, done);
+      }
+    } else {
+      //If the transition was triggered by anything in the nav bar or an unknown source, default to the crossfade
+      this._fade(from, to, done);
     }
+  }
 
-    /**
-     * Called when transitioning into a page
-     * 
-     * @param {*} from - the section being transitioned from
-     * @param {*} to - the section being transitioned to
-     * @param {*} done - the done callback
-     * @param {*} trigger - The trigger of the transition
-     */
-    in({from, to, done, trigger}) {
-        //If the transition was triggered by the back/forward buttons or a link not in the nav bar
-        if(trigger === 'popstate' || (!trigger.classList.contains("hamburger-link") && !trigger.classList.contains("logo-link"))) {
-            //get the names of the to and from sections
-            const toName = to.getAttribute("data-router-view");
-            const fromName = from.getAttribute("data-router-view");
-            
-            //Create booleans regarding the to namme
-            const isToHome = toName === "home";
-            const isToAbout = toName === "about";
-            const isToProjects = toName === "projects";
-            const isToContact = toName === "contact";
-            
-            //create booleans regarding the from name
-            const isFromHome = fromName === "home";
-            const isFromAbout = fromName === "about";
-            const isFromProjects = fromName === "projects";
-            const isFromContact = fromName === "contact";
-            
-            //determine the animation based on the to and from pages
-            if(isFromHome && isToProjects) {
-                this._slideUp(from, to, done);
-            } else if((isFromHome && isToAbout) || (isFromContact && isToHome)) {
-                //right
-                this._slideRight(from, to, done);
-            } else if(isFromProjects && isToHome) {
-                //down
-                this._slideDown(from, to, done);
-            } else if((isFromAbout && isToHome) || (isFromHome && isToContact)) {
-                //left
-                this._slideLeft(from, to, done);
-            } else {
-                //default to fade
-                this._fade(from, to, done)
-            }
-        } else {
-            //If the transition was trigger by anything in the nav bar or an unknown source, default to the crossfade
-            this._fade(from, to, done)
-        }
-
-        
-    }
-
-    /**
-     * Called when the page is being left
-     * @param {*} from - the section being left
-     * @param {*} done - the done callback 
-     */
-    out({from, done}) {
-        //All animations are done in the "in" method, so just call the done callback and return
-        done();
-    }
+  /**
+   * Called when the page is being left
+   * @param {*} from - the section being left
+   * @param {*} done - the done callback
+   */
+  out({ from, done }) {
+    //All animations are done in the "in" method, so just call the done callback and return
+    done();
+  }
 }
 
 export default Transitioner;
